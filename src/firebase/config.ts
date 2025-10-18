674becd72b6ccda37f09cdee0fc97d1342b540d4
Budha Vaishnava Suresh Budhil 2024021474 <vbudha@student.gitam.edu>
2025-10-18 18:56:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6560732135-c4764",
+  "appId": "1:493519766207:web:7ab5b4941abe1fbff83c0a",
+  "apiKey": "AIzaSyBXNrahmCq5_CwQ9HDrcoWr-AbDEUEZVlw",
+  "authDomain": "studio-6560732135-c4764.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "493519766207"
+};
